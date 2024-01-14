e698eda14bc61df72a06820756a3f080cee7814b
Yulian Prodanov <julianprodanov@icloud.com>
2024-01-14 21:39:35 +0200

    ui improvements of char selection

--- app/components/characterCard.tsx
+++ app/components/characterCard.tsx
@@ -1,9 +1,9 @@
-import { Box, Card, Stack, Typography } from "@mui/material";
+import { Box, Card, Grid, Stack, Typography } from "@mui/material";
 import Image from "next/image";
 import AutoFixHighIcon from "@mui/icons-material/AutoFixHigh";
 import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
 
-type Character = {
+export type Character = {
   name: string;
   title: string;
   picture: string;
@@ -11,33 +11,35 @@ type Character = {
 
 function CharacterCard({ name, title, picture }: Character) {
   return (
-    <Card
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        width: "20rem",
-        justifyContent: "space-between",
-      }}
-    >
-      <Box
+    <Grid item xs={8}>
+      <Card
         sx={{
-          padding: "1.2rem",
           display: "flex",
-          flexDirection: "column",
+          flexDirection: "row",
+          width: "20rem",
           justifyContent: "space-between",
         }}
       >
-        <Stack direction="column">
-          <Typography variant="h6">{name}</Typography>
-          <Typography variant="subtitle2">{title}</Typography>
-        </Stack>
-        <Stack direction="row" gap={1.5}>
-          <AutoFixHighIcon fontSize="small" color="action" />
-          <HealthAndSafetyIcon fontSize="small" color="action" />
-        </Stack>
-      </Box>
-      <Image src={picture} alt="char-img" width="150" height="64" />
-    </Card>
+        <Box
+          sx={{
+            padding: "1.2rem",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <Stack direction="column">
+            <Typography variant="h6">{name}</Typography>
+            <Typography variant="subtitle2">{title}</Typography>
+          </Stack>
+          <Stack direction="row" gap={1.5}>
+            <AutoFixHighIcon fontSize="small" color="action" />
+            <HealthAndSafetyIcon fontSize="small" color="action" />
+          </Stack>
+        </Box>
+        <Image src={picture} alt="char-img" width="150" height="64" />
+      </Card>
+    </Grid>
   );
 }
 export default CharacterCard;
--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,20 +1,89 @@
-import { Box } from "@mui/material/index";
-import CharacterCard from "../components/characterCard";
+import { Box, Grid, Stack } from "@mui/material/index";
+import CharacterCard, { Character } from "../components/characterCard";
 import SelectionOverlay from "../components/selectionOverlay";
 
+const characters: Character[] = [
+  {
+    name: "Raye",
+    title: "The Spice Master",
+    picture: "/characters/4.png",
+  },
+  {
+    name: "Test1",
+    title: "Test1 title",
+    picture: "/characters/1.png",
+  },
+  {
+    name: "Test2",
+    title: "Test2 title",
+    picture: "/characters/2.png",
+  },
+  {
+    name: "Test3",
+    title: "Test3 title",
+    picture: "/characters/3.png",
+  },
+  {
+    name: "Test4",
+    title: "Test4 title",
+    picture: "/characters/4.png",
+  },
+  {
+    name: "Test4",
+    title: "Test4 title",
+    picture: "/characters/4.png",
+  },
+  {
+    name: "Test4",
+    title: "Test4 title",
+    picture: "/characters/4.png",
+  },
+  {
+    name: "Test4",
+    title: "Test4 title",
+    picture: "/characters/4.png",
+  },
+  {
+    name: "Test4",
+    title: "Test4 title",
+    picture: "/characters/4.png",
+  },
+  {
+    name: "Test4",
+    title: "Test4 title",
+    picture: "/characters/4.png",
+  },
+];
+
 function Game() {
+  const charactersBox = characters.map((character, index) => {
+    return (
+      <CharacterCard
+        key={index}
+        name={character.name}
+        title={character.title}
+        picture={character.picture}
+      />
+    );
+  });
+
   return (
     <Box
       sx={{
         display: "flex",
         flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
-      <CharacterCard
-        name="Raye"
-        title="The Spice Master"
-        picture="/characters/4.png"
-      />
+      <Grid
+        sx={{ justifyContent: "center" }}
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 22, lg: 24, xl: 31 }}
+      >
+        {charactersBox}
+      </Grid>
       <SelectionOverlay />
     </Box>
   );
